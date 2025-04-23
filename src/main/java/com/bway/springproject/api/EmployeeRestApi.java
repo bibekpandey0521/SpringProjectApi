c08fc75198d30c04134afd3915f5d3addf1001f2
bibekpandey0521 <bibekpandey276@gmail.com>
2025-04-23 13:34:46 +0545

    Today i learned to fetch data from api and put into database and next i will learn to   implement throgh swagger api

--- src/main/java/com/bway/springproject/api/EmployeeRestApi.java
+++ src/main/java/com/bway/springproject/api/EmployeeRestApi.java
@@ -10,13 +10,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.bway.springproject.model.Employee;
+import com.bway.springproject.model.Product;
+import com.bway.springproject.repository.ProductRepository;
 import com.bway.springproject.service.EmployeeService;
+
 import java.util.List;
 
 @RestController
 public class EmployeeRestApi {
+	
+	@Autowired
+	private ProductRepository prodRepo;
 
 	@Autowired
 	private EmployeeService empService;
@@ -52,5 +59,41 @@ public class EmployeeRestApi {
 		
 		empService.addEmp(emp);
 		return new ResponseEntity<>("Update Success",HttpStatus.OK);
+	}
+	
+	@GetMapping("/api/emp/j2o")
+	public String jsonToObject() {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		Employee emp = restTemplate.getForObject("http://localhost:8080/api/emp/5", Employee.class);	
+		
+		return "FirstName = " +emp.getFname();
+//		return "Address : "+emp.getAddress();
+	}
+	
+	@GetMapping("/api/emp/ja2oa")
+	public String jsonArraytoObjectArray() {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		Employee []  emps = restTemplate.getForObject("http://localhost:8080/api/emp/list", Employee[].class);
+		return "fname = "+emps[1].getAddress().getCity();
+//		return "fname = "+emps[1].getAddress().getCity();
+		
+	}
+	
+	@GetMapping("/api/emp/load")
+	public String loadProducts() {
+		
+		RestTemplate restTemplate = new RestTemplate();
+//		String url = "https://fakestoreapi.com/products";
+		Product[] plist =restTemplate.getForObject("https://fakestoreapi.com/products",Product[].class);
+		
+		prodRepo.saveAll(List.of(plist));
+//		for (Product p : plist) {
+//            addProduct(p);
+//        }
+		return "success";
 	}
 }
---
+++ src/main/java/com/bway/springproject/model/Product.java
@@ -0,0 +1,31 @@
+package com.bway.springproject.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="product_tbl")
+public class Product {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	private double price;
+	@Column(columnDefinition = "longtext")
+	private String description;
+	private String category;
+	private String image;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="rating_id")
+	private Rating rating;
+	
+}
